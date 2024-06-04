3bf1cde614774a3c28754b9439c80586a15c5d88
blltrx <rose@bellatrix.dev>
2024-06-04 16:57:16 +0100

    better error message where TODOIST_TOKEN is unset

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ mod tui;
 // long term i need to have some kind of queue for api requests to be handled non-blockingly. rn im just happy to have something useable
 
 fn main() -> io::Result<()> {
+    // read token
+    let token = match env::var("TODOIST_TOKEN") {
+        Err(_) => {
+            println!("Error reading token.\nPlease set the TODOIST_TOKEN environment variable to your API token found in your Todoist settings.\nGo to: https://app.todoist.com/app/settings/integrations/developer");
+            std::process::exit(1);
+        }
+        Ok(token) => token,
+    };
     // initialise terminal ready for render
     let mut terminal = tui::init()?;
     // initialise app and api client
-    let token = match env::var("TODOIST_TOKEN") {
-        Err(_) => panic!("Check that TODOIST_TOKEN environment variable is set"),
-        Ok(token) => token,
-    };
     let mut app_client = app::App::new(token);
     // run application
     let app_result = app_client.run(&mut terminal);
